2b1fc275776f18bb5c40f698e08a3743d6b4a1ca
tony <tony@madtec.net>
2015-05-14 10:26:58 +0800

    001

--- testgit/Form1.cs
+++ testgit/Form1.cs
@@ -16,5 +16,10 @@ namespace testgit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("dd");
+        }
     }
 }
